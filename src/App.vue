30de4e9d4689dc620fd630c61f49d624634f888e
pizzi9523 <pizzitolagiuseppe@live.it>
2022-03-22 18:15:42 +0100

    Create and add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <header>
+      <Nav />
+    </header>
+    <!-- /header  -->
+
+    <main></main>
+    <!-- /main  -->
+
+    <footer></footer>
+    <!-- /footer  -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import Nav from "./components/Nav.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+    Nav,
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,81 @@
+<template>
+  <nav class="Navbar py-4">
+    <div class="container">
+      <div class="d-flex justify-content-between align-items-center">
+        <div class="logo">
+          <a href="#">
+            <img src="../assets/logo.png" alt="" />
+          </a>
+        </div>
+        <!-- /logo  -->
+
+        <div class="search">
+          <input type="text" name="search" id="searchbar" placeholder="Cerca" />
+        </div>
+        <!-- /search  -->
+
+        <div class="menu d-flex">
+          <div class="followed px-2">
+            <a href="#">
+              <i class="fa-regular fa-heart"></i>
+            </a>
+          </div>
+          <div class="home px-2">
+            <a href="#">
+              <i class="fa-solid fa-house-chimney"></i>
+            </a>
+          </div>
+          <div class="profile px-2">
+            <a href="#">
+              <img src="../assets/profile.jpg" alt="" />
+            </a>
+          </div>
+        </div>
+        <!-- /menu  -->
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.Navbar {
+  border-bottom: 1px solid darkgray;
+  a {
+    color: black;
+    text-decoration: none !important;
+    i {
+      width: 24px;
+    }
+  }
+
+  .logo {
+    img {
+      width: 103px;
+    }
+  }
+
+  .search {
+    input {
+      padding: 0 10px;
+      border: 1px solid lightgray;
+      &::placeholder {
+        color: darkgray;
+        text-align: center;
+      }
+    }
+  }
+  .menu {
+    align-items: center;
+    .profile {
+      img {
+        width: 24px;
+        border-radius: 50%;
+      }
+    }
+  }
+}
+</style>
